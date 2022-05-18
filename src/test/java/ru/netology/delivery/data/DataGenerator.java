69cf770b635e303fa947dc86cb734bd9b33278cd
olgamasl <masloolg@gmail.com>
2022-05-18 13:24:16 +0300

    wrong

---
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -0,0 +1,53 @@
+package ru.netology.delivery.data;
+
+import com.github.javafaker.Faker;
+import lombok.NonNull;
+import lombok.Value;
+import lombok.experimental.UtilityClass;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+@UtilityClass
+public class DataGenerator {
+
+    //private DataGenerator() {
+    //}
+    private static Faker faker;
+
+    @BeforeEach
+    void setUpAll() {
+        faker = new Faker(new Locale("ru"));
+    }
+
+    public static String generateDate(int shift) {
+        String date = LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return date;
+    }
+
+    public static String generateCity(String locale) {
+        String city = faker.address().cityName();
+        return city;
+    }
+
+    public static String generateName(String locale) {
+        String name = faker.name().fullName();
+        return name;
+    }
+
+    public static String generatePhone(String locale) {
+        String phone = faker.phoneNumber().phoneNumber();
+        return phone;
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
---
+++ src/test/java/ru/netology/delivery/test/CardDelivery2Test.java
@@ -0,0 +1,44 @@
+package ru.netology.delivery.test;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import ru.netology.delivery.data.DataGenerator;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static java.awt.SystemColor.info;
+
+public class CardDelivery2Test {
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldSuccessfulPlanAndRePlanMeeting() {
+
+        //var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+
+        $x("[//*[@placeholder=\"Город\"]]").setValue(DataGenerator.generateCity("ru"));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(String.valueOf(daysToAddForFirstMeeting));
+        $x("//*[@name=\"name\"]").setValue(DataGenerator.generateName("ru"));
+        $x("//*[@name=\"phone\"]").setValue(DataGenerator.generatePhone("ru"));
+
+        $("[data-test-id=\"agreement\"]").click();
+        $(withText("Запланировать")).click();
+        $("[data-test-id=\"success-notification\"]")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate), Duration.ofSeconds(15));
+    }
+}
